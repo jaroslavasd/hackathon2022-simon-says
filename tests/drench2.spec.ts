a96bbaf23aff8f7277af0ee101224bb00c9da611
jaroslavasd <jaroslavasd@wix.com>
2022-10-24 17:14:11 +0300

    add simon

---
+++ tests/drench2.spec.ts
@@ -0,0 +1,59 @@
+import { test } from "@playwright/test";
+
+const games = 15;
+
+test("Play The Game", async ({ page }) => {
+	await page.goto("https://weslleyaraujo.github.io/react-simon-says/");
+	await page.getByRole("link", { name: "Play" }).click();
+
+	let observationTimeout = 1500;
+	for (let i = 0; i < games; i++) {
+		observationTimeout = observationTimeout + 450;
+		const sequence = await observe(page, observationTimeout);
+		await clickItems(page, sequence);
+	}
+	await new Promise(() => {});
+});
+
+const observe = async (page, observationTimeout) => {
+	return await page.evaluate(async (observationTimeout) => {
+		const targetNode = document.querySelector("#root");
+		const config = { attributes: true, subtree: true };
+
+		const items: string[] = [];
+
+		const func = (mutationList) => {
+			for (const mutation of mutationList) {
+				if (mutation.target.id) {
+					items.push(mutation.target.id);
+				}
+			}
+		};
+
+		const observer = new MutationObserver(func);
+		observer.observe(targetNode!, config);
+
+		return await new Promise((resolve) => {
+			setTimeout(() => {
+				observer.disconnect();
+				resolve(items);
+			}, observationTimeout);
+		});
+	}, observationTimeout);
+};
+
+const clickItems = async (page, items) => {
+	const odds = items.filter((value, index) => {
+		return index % 2 !== 0;
+	});
+
+	console.log("Items to click: " + odds);
+	for (let i = 0; i < odds.length; i++) {
+		const selector = odds[i];
+		console.log("Clicking:" + selector);
+		await page.click(`#${selector}`);
+		await page.evaluate(async () => {
+			setTimeout(function () {}, 50);
+		});
+	}
+};
